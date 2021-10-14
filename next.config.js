9bf6e5eba2ef652cf613befb6cf9653de35814d8
AP-DAVID <akintolaapre@gmail.com>
2021-10-14 22:48:44 +0100

    First commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const path = require("path");
+
+module.exports = withPlugins([[withImages]], {
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve("./"));
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  },
+  
+  target: 'serverless'
+});
+
+
